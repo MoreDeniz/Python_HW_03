40a475e3a7b5a91aa72e0e74b74dda626f881e16
Mayya <msalosina@mail.ru>
2023-01-19 21:06:16 +0700

    Ускоренная обработка Multiplying_pairs_of_umbers

--- Ex_002_Multiplying_pairs_of_numbers.py
+++ Ex_002_Multiplying_pairs_of_numbers.py
@@ -4,26 +4,41 @@
 # [2, 3, 4, 5, 6] => [12, 15, 16];
 # [2, 3, 5, 6] => [12, 15]
 
-def get_list():
-    my_list = []
-    size = int(input('Enter number: '))
-    for i in range(size):
-        my_list.append(random.randint(0, 9))
-    return my_list
+# def get_list():
+#     my_list = []
+#     size = int(input('Enter number: '))
+#     for i in range(size):
+#         my_list.append(random.randint(0, 9))
+#     return my_list
 
-def multyplying(my_list):
-    multy_list = []
-    if len(my_list) % 2 == 0:
-        for i in range(0, int(len(my_list)/2), 1):
-            multy_list.append(my_list[i] * my_list[len(my_list) - 1 -i])            
-    else:
-        for i in range(0, int(len(my_list) / 2 + 1), 1):
-            multy_list.append(my_list[i] * my_list[len(my_list) - 1 -i]) 
-    return multy_list
+# def multyplying(my_list):
+#     multy_list = []
+#     if len(my_list) % 2 == 0:
+#         for i in range(0, int(len(my_list)/2), 1):
+#             multy_list.append(my_list[i] * my_list[len(my_list) - 1 -i])            
+#     else:
+#         for i in range(0, int(len(my_list) / 2 + 1), 1):
+#             multy_list.append(my_list[i] * my_list[len(my_list) - 1 -i]) 
+#     return multy_list
 
-import random
+# import random
 
-my_list = get_list()
-multy_list = multyplying(my_list)
-print(f'{my_list} => {multy_list}')
+# my_list = get_list()
+# multy_list = multyplying(my_list)
+# print(f'{my_list} => {multy_list}')
 
+from random import randint as RI
+
+my_list = [RI(0, 10) for _ in range(RI(3, 10))]
+
+if len(my_list) % 2 == 0:
+    left_list = [my_list[i] for i in range(len(my_list) // 2)]
+    right_list = list([my_list[len(my_list) - 1 -i] for i in range(len(my_list) // 2)])
+else:
+    left_list = [my_list[i] for i in range(len(my_list) // 2 + 1)]
+    right_list = list([my_list[len(my_list) - 1 -i] for i in range(len(my_list) // 2 + 1)])
+
+a = []
+for i, j in zip(left_list, right_list):
+    a.append(i * j)
+print(f'{my_list} => {a}')

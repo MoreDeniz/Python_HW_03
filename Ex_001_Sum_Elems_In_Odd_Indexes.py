fd75833190b388b63688cf5e4aa23333b4d4490e
Mayya <msalosina@mail.ru>
2023-01-19 18:32:51 +0700

    Ускоренная обработка Sum Elem In Odd Indexes

--- Ex_001_Sum_Elems_In_Odd_Indexes.py
+++ Ex_001_Sum_Elems_In_Odd_Indexes.py
@@ -4,33 +4,44 @@
 # Пример:
 # [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
-def get_list():
-    my_list = []
-    size = int(input('Введите количество элементов списка: '))
-    for i in range(size):
-        my_list.append(random.randint(0, 9))
-    return my_list
+# def get_list():
+#     my_list = []
+#     size = int(input('Введите количество элементов списка: '))
+#     for i in range(size):
+#         my_list.append(random.randint(0, 9))
+#     return my_list
 
-def sum_elems_odd_indexes(my_list):
-    sum = 0
-    for i in range(len(my_list)):
-        if i % 2 != 0:
-            sum += my_list[i]
-    return sum
+# def sum_elems_odd_indexes(my_list):
+#     sum = 0
+#     for i in range(len(my_list)):
+#         if i % 2 != 0:
+#             sum += my_list[i]
+#     return sum
 
-def odd_index_elems(my_list):
-    odd_elems = []
-    for i in range(len(my_list)):
-        if i % 2 != 0:
-            odd_elems.append(my_list[i])
-    return odd_elems
+# def odd_index_elems(my_list):
+#     odd_elems = []
+#     for i in range(len(my_list)):
+#         if i % 2 != 0:
+#             odd_elems.append(my_list[i])
+#     return odd_elems
 
-import random
+# import random
 
-my_list = get_list()
-sum = sum_elems_odd_indexes(my_list)
-odd_elems = odd_index_elems(my_list)
+# my_list = get_list()
+# sum = sum_elems_odd_indexes(my_list)
+# odd_elems = odd_index_elems(my_list)
 
-print(my_list, end = '')
-print(f' -> на нечётных позициях элементы: {odd_elems}, ответ: {sum}')
+# print(my_list, end = '')
+# print(f' -> на нечётных позициях элементы: {odd_elems}, ответ: {sum}')
 
+from functools import reduce
+from random import randint as RI
+
+my_list = [RI(0, 20) for _ in range(10)]
+print(f'В списке: {my_list}')
+
+my_list = [my_list[i] for i in range(10) if i % 2 != 0]
+print(f'на нечётных позициях элементы: {my_list}')
+
+summa = reduce((lambda x, y: x + y), my_list)
+print(f'Их сумма: {summa}')
